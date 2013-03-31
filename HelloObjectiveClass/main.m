c8b3f947f340efd5982b8d3a8acab0cc136cdd6a
Gaurav <gaurava.2012@gmail.com>
2013-03-31 00:03:09 -0700

    Initial Commit

---
+++ HelloObjectiveClass/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  HelloObjectiveClass
+//
+//  Created by Gaurav Agrawal on 3/31/13.
+//  Copyright (c) 2013 Gaurav Agrawal. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
